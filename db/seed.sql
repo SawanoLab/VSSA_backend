6f2db8d32eabf37291efe665b78d2ec75ad4f155
fuki <55621861+fuki01@users.noreply.github.com>
2023-11-15 16:28:47 +0900

    Add PlayerDelete and PlayerUpdate endpoints to player router

--- db/seed.sql
+++ db/seed.sql
@@ -1,4 +1,4 @@
-INSERT INTO users (uuid, username) VALUES ( 'aa887aa657db4da0b7f3bc52f9d8898e','username' );
+INSERT INTO users (uuid, username) VALUES ( '3be043a01d954ddb81ca5db72d7d40a3','username' );
 
 INSERT INTO seasons (created_at, updated_at, start_day, end_day, season_name, game_format, code, user_id, uuid)
 VALUES (
@@ -10,7 +10,7 @@ VALUES (
     'Summer 2022', -- season_name
     'Game Format Example', -- game_format
     'S2022_001', -- code
-    'aa887aa657db4da0b7f3bc52f9d8898e', -- user_id
+    '3be043a01d954ddb81ca5db72d7d40a3', -- user_id
     '89650f78c57247039d385b57aa59db34' -- uuid
 );
 
@@ -25,7 +25,7 @@ VALUES
     'Coach 1', -- coach
     'Trainer 1', -- trainer
     'Doctor 1', -- doctor
-    'aa887aa657db4da0b7f3bc52f9d8898e', -- user_id
+    '3be043a01d954ddb81ca5db72d7d40a3', -- user_id
     '89650f78c57247039d385b57aa59db34', -- season_id
     'edf400dc06c641cfbec67d8d4ba82e3f' -- uuid
 ),
@@ -38,31 +38,31 @@ VALUES
     'Coach 2', -- coach
     'Trainer 2', -- trainer
     'Doctor 2', -- doctor
-    'aa887aa657db4da0b7f3bc52f9d8898e', -- user_id
+    '3be043a01d954ddb81ca5db72d7d40a3', -- user_id
     '89650f78c57247039d385b57aa59db34', -- season_id
     '329a1c1570c543cc8750386b961a1df6' -- uuid
-)
+);
 
 
 INSERT INTO players(uuid, name, player_number, code, postion, weight, height, user_id, team_id, season_id)
 VALUES
-('68ef9a7adae2456f891c831b04901b8f', 'Player 1', 1, 'PL1', 'Wing Spiker', 75, 180, 'aa887aa657db4da0b7f3bc52f9d8898e', 'edf400dc06c641cfbec67d8d4ba82e3f', '89650f78c57247039d385b57aa59db34'),
-('68ef9a7adae2456f891c831b04901b9f', 'Player 2', 2, 'PL2', 'Middle Blocker', 80, 185, 'aa887aa657db4da0b7f3bc52f9d8898e', 'edf400dc06c641cfbec67d8d4ba82e3f', '89650f78c57247039d385b57aa59db34'),
-('68ef9a7adae2456f891c831b04901b1f', 'Player 3', 3, 'PL3', 'Setter', 70, 175, 'aa887aa657db4da0b7f3bc52f9d8898e', 'edf400dc06c641cfbec67d8d4ba82e3f', '89650f78c57247039d385b57aa59db34'),
-('68ef9a7adae2456f891c831b04901b2f', 'Player 4', 4, 'PL4', 'Wing Spiker', 78, 182, 'aa887aa657db4da0b7f3bc52f9d8898e', 'edf400dc06c641cfbec67d8d4ba82e3f', '89650f78c57247039d385b57aa59db34'),
-('68ef9a7adae2456f891c831b04901b3f', 'Player 5', 5, 'PL5', 'Middle Blocker', 81, 186, 'aa887aa657db4da0b7f3bc52f9d8898e', 'edf400dc06c641cfbec67d8d4ba82e3f', '89650f78c57247039d385b57aa59db34'),
-('68ef9a7adae2456f891c831b04901b4f', 'Player 6', 6, 'PL6', 'Opposite', 77, 181, 'aa887aa657db4da0b7f3bc52f9d8898e', 'edf400dc06c641cfbec67d8d4ba82e3f', '89650f78c57247039d385b57aa59db34'),
-('68ef9a7adae2456f891c831b04901b5f', 'Player 7', 7, 'PL7', 'Libero', 67, 172, 'aa887aa657db4da0b7f3bc52f9d8898e', 'edf400dc06c641cfbec67d8d4ba82e3f', '89650f78c57247039d385b57aa59db34'),
-('68ef9a7adae2456f891c831b04901b6f', 'Player 8', 8, 'PL8', 'Wing Spiker', 76, 179, 'aa887aa657db4da0b7f3bc52f9d8898e', 'edf400dc06c641cfbec67d8d4ba82e3f', '89650f78c57247039d385b57aa59db34'),
-('68ef9a7adae2456f891c831b04901b7f', 'Player 9', 9, 'PL9', 'Middle Blocker', 80, 184, 'aa887aa657db4da0b7f3bc52f9d8898e', 'edf400dc06c641cfbec67d8d4ba82e3f', '89650f78c57247039d385b57aa59db34'),
-('18ef9a7adae2456f891c831b04901b8f', 'Player 10', 10, 'PL10', 'Setter', 72, 176, 'aa887aa657db4da0b7f3bc52f9d8898e', 'edf400dc06c641cfbec67d8d4ba82e3f', '89650f78c57247039d385b57aa59db34'),
-('28ef9a7adae2456f891c831b04901b8f', 'Player 11', 11, 'PL11', 'Wing Spiker', 74, 178, 'aa887aa657db4da0b7f3bc52f9d8898e', '329a1c1570c543cc8750386b961a1df6', '89650f78c57247039d385b57aa59db34'),
-('38ef9a7adae2456f891c831b04901b8f', 'Player 12', 12, 'PL12', 'Middle Blocker', 82, 186, 'aa887aa657db4da0b7f3bc52f9d8898e', '329a1c1570c543cc8750386b961a1df6', '89650f78c57247039d385b57aa59db34'),
-('48ef9a7adae2456f891c831b04901b8f', 'Player 13', 13, 'PL13', 'Opposite', 75, 180, 'aa887aa657db4da0b7f3bc52f9d8898e', '329a1c1570c543cc8750386b961a1df6', '89650f78c57247039d385b57aa59db34'),
-('58ef9a7adae2456f891c831b04901b8f', 'Player 14', 14, 'PL14', 'Libero', 69, 171, 'aa887aa657db4da0b7f3bc52f9d8898e', '329a1c1570c543cc8750386b961a1df6', '89650f78c57247039d385b57aa59db34'),
-('68ef9a7adae2456f891c831b14901b8f', 'Player 15', 15, 'PL15', 'Wing Spiker', 73, 177, 'aa887aa657db4da0b7f3bc52f9d8898e', '329a1c1570c543cc8750386b961a1df6', '89650f78c57247039d385b57aa59db34'),
-('78ef9a7adae2456f891c831b04901b8f', 'Player 16', 16, 'PL16', 'Middle Blocker', 79, 183, 'aa887aa657db4da0b7f3bc52f9d8898e', '329a1c1570c543cc8750386b961a1df6', '89650f78c57247039d385b57aa59db34'),
-('88ef9a7adae2456f891c831b04901b8f', 'Player 17', 17, 'PL17', 'Setter', 72, 174, 'aa887aa657db4da0b7f3bc52f9d8898e', '329a1c1570c543cc8750386b961a1df6', '89650f78c57247039d385b57aa59db34'),
-('98ef9a7adae2456f891c831b04901b8f', 'Player 18', 18, 'PL18', 'Wing Spiker', 74, 177, 'aa887aa657db4da0b7f3bc52f9d8898e', '329a1c1570c543cc8750386b961a1df6', '89650f78c57247039d385b57aa59db34'),
-('61ef9a7adae2456f891c831b04901b8f', 'Player 19', 19, 'PL19', 'Middle Blocker', 80, 185, 'aa887aa657db4da0b7f3bc52f9d8898e', '329a1c1570c543cc8750386b961a1df6', '89650f78c57247039d385b57aa59db34'),
-('62ef9a7adae2456f891c831b04901b8f', 'Player 20', 20, 'PL20', 'Opposite', 78, 182, 'aa887aa657db4da0b7f3bc52f9d8898e', '329a1c1570c543cc8750386b961a1df6', '89650f78c57247039d385b57aa59db34');
+('68ef9a7adae2456f891c831b04901b8f', 'Player 1', 1, 'PL1', 'Wing Spiker', 75, 180, '3be043a01d954ddb81ca5db72d7d40a3', 'edf400dc06c641cfbec67d8d4ba82e3f', '89650f78c57247039d385b57aa59db34'),
+('68ef9a7adae2456f891c831b04901b9f', 'Player 2', 2, 'PL2', 'Middle Blocker', 80, 185, '3be043a01d954ddb81ca5db72d7d40a3', 'edf400dc06c641cfbec67d8d4ba82e3f', '89650f78c57247039d385b57aa59db34'),
+('68ef9a7adae2456f891c831b04901b1f', 'Player 3', 3, 'PL3', 'Setter', 70, 175, '3be043a01d954ddb81ca5db72d7d40a3', 'edf400dc06c641cfbec67d8d4ba82e3f', '89650f78c57247039d385b57aa59db34'),
+('68ef9a7adae2456f891c831b04901b2f', 'Player 4', 4, 'PL4', 'Wing Spiker', 78, 182, '3be043a01d954ddb81ca5db72d7d40a3', 'edf400dc06c641cfbec67d8d4ba82e3f', '89650f78c57247039d385b57aa59db34'),
+('68ef9a7adae2456f891c831b04901b3f', 'Player 5', 5, 'PL5', 'Middle Blocker', 81, 186, '3be043a01d954ddb81ca5db72d7d40a3', 'edf400dc06c641cfbec67d8d4ba82e3f', '89650f78c57247039d385b57aa59db34'),
+('68ef9a7adae2456f891c831b04901b4f', 'Player 6', 6, 'PL6', 'Opposite', 77, 181, '3be043a01d954ddb81ca5db72d7d40a3', 'edf400dc06c641cfbec67d8d4ba82e3f', '89650f78c57247039d385b57aa59db34'),
+('68ef9a7adae2456f891c831b04901b5f', 'Player 7', 7, 'PL7', 'Libero', 67, 172, '3be043a01d954ddb81ca5db72d7d40a3', 'edf400dc06c641cfbec67d8d4ba82e3f', '89650f78c57247039d385b57aa59db34'),
+('68ef9a7adae2456f891c831b04901b6f', 'Player 8', 8, 'PL8', 'Wing Spiker', 76, 179, '3be043a01d954ddb81ca5db72d7d40a3', 'edf400dc06c641cfbec67d8d4ba82e3f', '89650f78c57247039d385b57aa59db34'),
+('68ef9a7adae2456f891c831b04901b7f', 'Player 9', 9, 'PL9', 'Middle Blocker', 80, 184, '3be043a01d954ddb81ca5db72d7d40a3', 'edf400dc06c641cfbec67d8d4ba82e3f', '89650f78c57247039d385b57aa59db34'),
+('18ef9a7adae2456f891c831b04901b8f', 'Player 10', 10, 'PL10', 'Setter', 72, 176, '3be043a01d954ddb81ca5db72d7d40a3', 'edf400dc06c641cfbec67d8d4ba82e3f', '89650f78c57247039d385b57aa59db34'),
+('28ef9a7adae2456f891c831b04901b8f', 'Player 11', 11, 'PL11', 'Wing Spiker', 74, 178, '3be043a01d954ddb81ca5db72d7d40a3', '329a1c1570c543cc8750386b961a1df6', '89650f78c57247039d385b57aa59db34'),
+('38ef9a7adae2456f891c831b04901b8f', 'Player 12', 12, 'PL12', 'Middle Blocker', 82, 186, '3be043a01d954ddb81ca5db72d7d40a3', '329a1c1570c543cc8750386b961a1df6', '89650f78c57247039d385b57aa59db34'),
+('48ef9a7adae2456f891c831b04901b8f', 'Player 13', 13, 'PL13', 'Opposite', 75, 180, '3be043a01d954ddb81ca5db72d7d40a3', '329a1c1570c543cc8750386b961a1df6', '89650f78c57247039d385b57aa59db34'),
+('58ef9a7adae2456f891c831b04901b8f', 'Player 14', 14, 'PL14', 'Libero', 69, 171, '3be043a01d954ddb81ca5db72d7d40a3', '329a1c1570c543cc8750386b961a1df6', '89650f78c57247039d385b57aa59db34'),
+('68ef9a7adae2456f891c831b14901b8f', 'Player 15', 15, 'PL15', 'Wing Spiker', 73, 177, '3be043a01d954ddb81ca5db72d7d40a3', '329a1c1570c543cc8750386b961a1df6', '89650f78c57247039d385b57aa59db34'),
+('78ef9a7adae2456f891c831b04901b8f', 'Player 16', 16, 'PL16', 'Middle Blocker', 79, 183, '3be043a01d954ddb81ca5db72d7d40a3', '329a1c1570c543cc8750386b961a1df6', '89650f78c57247039d385b57aa59db34'),
+('88ef9a7adae2456f891c831b04901b8f', 'Player 17', 17, 'PL17', 'Setter', 72, 174, '3be043a01d954ddb81ca5db72d7d40a3', '329a1c1570c543cc8750386b961a1df6', '89650f78c57247039d385b57aa59db34'),
+('98ef9a7adae2456f891c831b04901b8f', 'Player 18', 18, 'PL18', 'Wing Spiker', 74, 177, '3be043a01d954ddb81ca5db72d7d40a3', '329a1c1570c543cc8750386b961a1df6', '89650f78c57247039d385b57aa59db34'),
+('61ef9a7adae2456f891c831b04901b8f', 'Player 19', 19, 'PL19', 'Middle Blocker', 80, 185, '3be043a01d954ddb81ca5db72d7d40a3', '329a1c1570c543cc8750386b961a1df6', '89650f78c57247039d385b57aa59db34'),
+('62ef9a7adae2456f891c831b04901b8f', 'Player 20', 20, 'PL20', 'Opposite', 78, 182, '3be043a01d954ddb81ca5db72d7d40a3', '329a1c1570c543cc8750386b961a1df6', '89650f78c57247039d385b57aa59db34');
